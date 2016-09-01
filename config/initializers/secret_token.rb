ab319a7298f4d2fb42a6d26f6cf3b9d774eafc3c
Denis Sidorenko <denis.sidorenko@kodep.ru>
2016-09-01 22:42:54 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fifteenbucks::Application.config.secret_key_base = '38352c79cbcdaee2177b021426c5ff26d7943bf8cc768e08d534f38f8b86528525b8fcd6494f72a2235bf68f21d3263f4c8e0aced362306c7c5c10e44eb01f87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fifteenbucks::Application.config.session_store :cookie_store, key: '_fifteenbucks_session'
